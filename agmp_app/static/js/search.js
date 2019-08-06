033a0a959a4ab096634dcd0fd5d78e55c249d7a6
Ebo Turkson <mturkson23@gmail.com>
2019-08-06 13:25:49 +0000

    modify search with project specific requirements

---
+++ agmp_app/static/js/search.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+  $("input[name='search_type_check']").change(function () {
+    // define array to hold selected category types
+    let cats = [];
+    let placeholder;
+    // on category (remember its a checkbox) change
+    // push the selected category value into the category array
+    $.each($("input[name='search_type_check']:checked"), function () {
+      cats.push($(this).val());
+    });
+    // disable search button on empty categories selected
+    if (cats.length < 1) {
+      $(".search-btn").attr("disabled", true);
+    } else {
+      $(".search-btn").attr("disabled", false);
+    }
+    // updatePlaceholder(cats);
+    if (cats.length == 0) {
+      placeholder = '';
+    } else {
+      placeholder = 'Enter ' + cats.join(', ') + ' separated by double colon (::)';
+    }
+    $("input[type=text], #search").attr('placeholder', placeholder);
+  });
+
+  $("#search-form").submit(function (event) {
+    event.preventDefault();
+  });
+});
