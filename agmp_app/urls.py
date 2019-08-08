004da534bbef55dfcb1f12abfba0ca7c5eb17be7
Ebo Turkson <mturkson23@gmail.com>
2019-08-08 08:11:43 +0000

    add query definition

--- agmp_app/urls.py
+++ agmp_app/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('search/', views.search, name='search'),
+
+    # call search query with optional parameters 
+    path('search/<str:query_string>', views.query, kwargs={'disease': 0, 'drug': 0, 'variant': 0, 'gene': 0}, name='query'),
     path('search_details/<str:db_name>/<int:query_id>', views.search_details, name='search_details'),
     path('resources/', views.resources, name='resources'),
     path('contact/', views.contact, name='contact'),
