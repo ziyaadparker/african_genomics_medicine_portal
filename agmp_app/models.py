29c0599e499ee24d51fb8a65edb4b02b90750d10
AlfredUg <assekagiri@gmail.com>
2019-10-01 14:22:33 +0300

    Add updated models and db

--- agmp_app/models.py
+++ agmp_app/models.py
@@ -2,71 +2,70 @@ from django.db import models
 from django.utils import timezone
 from decimal import Decimal
 
-# Create your models here.    
-class drug_state(models.Model):
-    state_id=models.AutoField(primary_key=True)
-    state_name=models.CharField(max_length=250)
 
 class drug(models.Model):
-    drug_id=models.AutoField(primary_key=True)
+    id=models.CharField(max_length=250, primary_key=True)
     drug_name=models.CharField(max_length=250)
-    posology=models.CharField(max_length=250, default="NA")
-    chemical_structure=models.TextField(default="NA")
-    state=models.ForeignKey(drug_state, on_delete=models.CASCADE)
     drug_bank_id=models.IntegerField(default=0) #may have to link to another table
-
-class externalid_type(models.Model):
-    type_id=models.AutoField(primary_key=True)
-    type_name=models.CharField(max_length=250)
+    #state=models.ForeignKey(drug_state, on_delete=models.CASCADE)
+    state=models.CharField(max_length=250)
+    indication=models.CharField(default="NA",max_length=50)
+    iupac_name=models.CharField(default="NA",max_length=50)
 
 class study(models.Model):
-    study_id=models.AutoField(primary_key=True)
-    study_title=models.CharField(max_length=250)
-    #externalid_type=models.ForeignKey(externalid_type, on_delete=models.CASCADE) #we need to check if we really need this in here
+    id=models.CharField(max_length=250, primary_key=True)
+    title=models.CharField(max_length=250)
     external_id=models.CharField(max_length=50)
+    type=models.CharField(max_length=250)
     date=models.DateField(default=timezone.now) #we are using current date to hold this space, should change this when using real data
 
 class pharmacogenes(models.Model):
-    id=models.AutoField(primary_key=True)
-    gene_name=models.CharField(max_length=50)
+    id=models.CharField(max_length=250, primary_key=True)
+    gene_name=models.CharField(max_length=50, default="NA")
     protein=models.CharField(max_length=50, default="NA")
-    function=models.TextField(default="NA")
+    function=models.TextField(default="NA",max_length=50)
+    chromosome=models.CharField(default="NA",max_length=50)
 
-class country_ethnicity(models.Model):
-    id=models.AutoField(primary_key=True)
-    country_ethnicity=models.CharField(max_length=150)
-
-#we may have to change disease_phenotype to just phenotype
-#since some of the phenotypes we look at there are not necessarily diseases
 class snp(models.Model):
-    snp_id=models.AutoField(primary_key=True)
-    rs_id=models.CharField(max_length=50)
-    drug=models.ForeignKey(drug, on_delete=models.CASCADE, default=1)
-    allele=models.CharField(max_length=50)
-    gene=models.ForeignKey(pharmacogenes, on_delete=models.CASCADE,default=2)
-    disease_phenotype=models.TextField()
-    reference=models.ForeignKey(study, on_delete=models.CASCADE,default=3)
-    chromosome=models.CharField(max_length=50)
-    p_value=models.DecimalField(max_digits=10,decimal_places=4,default=Decimal('0.0000'))
-
-class snp_country_ethnicity(models.Model):
     id=models.AutoField(primary_key=True)
-    snp_id=models.ForeignKey(snp, on_delete=models.CASCADE)
-    country_ethnicity_id=models.ForeignKey(country_ethnicity, on_delete=models.CASCADE)
+    snp_id=models.CharField(max_length=50, default='NA')
+    rs_id=models.CharField(max_length=50)
+    drug=models.ForeignKey(drug, on_delete=models.CASCADE, default="DRUG1")
+    allele=models.CharField(max_length=50)
+    gene=models.ForeignKey(pharmacogenes, on_delete=models.CASCADE,default="PHARGENE19")
+    phenotype=models.TextField()
+    reference=models.ForeignKey(study, on_delete=models.CASCADE,default="REF1")
+    p_value=models.CharField(max_length=10)
+    #p_value=models.DecimalField(max_digits=10,decimal_places=4,default=Decimal('0.0000'))
+    source=models.CharField(max_length=50)
+    id_in_source=models.CharField(max_length=50)
+    chromosome=models.CharField(max_length=50)
 
 class star_allele(models.Model):
     id=models.AutoField(primary_key=True)
+    star_id=models.CharField(max_length=250, default='NA') 
     star_annotation=models.CharField(max_length=50)
-    drug=models.ForeignKey(drug, on_delete=models.CASCADE)
-    gene=models.ForeignKey(pharmacogenes, on_delete=models.CASCADE)
-    disease_phenotype=models.CharField(max_length=50)
-    reference=models.ForeignKey(study, on_delete=models.CASCADE)
+    drug=models.ForeignKey(drug, on_delete=models.CASCADE, default="DRUG1")
+    allele=models.CharField(max_length=50)
+    gene=models.ForeignKey(pharmacogenes, on_delete=models.CASCADE,default="PHARGENE19")
+    phenotype=models.CharField(max_length=50)
+    reference=models.ForeignKey(study, on_delete=models.CASCADE, default="REF1")
+    p_value=models.CharField(max_length=10)
+    #p_value=models.DecimalField(max_digits=10,decimal_places=4,default=Decimal('0.0000'))
+    source=models.CharField(max_length=50)
+    id_in_source=models.CharField(max_length=50)
     chromosome=models.CharField(max_length=50)
-    p_value=models.DecimalField(max_digits=10,decimal_places=4,default=Decimal('0.0000'))
+
+class snp_ethnicity_country(models.Model):
+    id=models.CharField(max_length=250, primary_key=True)
+    snp=models.CharField(max_length=50)
+    #snp=models.ForeignKey(snp, on_delete=models.CASCADE)
+    region=models.CharField(max_length=50)
+    country_of_participants=models.CharField(max_length=50)
   
 class star_allele_ethnicity_country(models.Model):
-    id=models.AutoField(primary_key=True)
-    star_allele_id=models.ForeignKey(star_allele, on_delete=models.CASCADE)
-    country_ethnicity_id=models.ForeignKey(country_ethnicity, on_delete=models.CASCADE)
-
-
+    id=models.CharField(max_length=250, primary_key=True)
+    star_allele=models.CharField(max_length=50)
+    #star_allele=models.ForeignKey(star_allele, on_delete=models.CASCADE)
+    region=models.CharField(max_length=50)
+    country_of_participants=models.CharField(max_length=50)
